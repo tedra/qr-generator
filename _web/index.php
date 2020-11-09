c8ad1beb2ebda97751626d35466f25fbce5f5e8a
Tedra <dl@tedra.net>
2020-11-09 23:41:50 +0000

    X

--- _web/index.php
+++ _web/index.php
@@ -8,6 +8,7 @@ include('../_includes/setup.php');
     <link rel="stylesheet" href="/css/bootstrap.min.css" crossorigin="anonymous">
     <title>filter.ar</title>
     <script src="https://code.jquery.com/jquery.min.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/88ea9bd1aa.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="/js/bootstrap.min.js" crossorigin="anonymous"></script>
   </head>
