dd28dcf9cb856c33ab1ca9075312df038650494f
Tedra <dl@tedra.net>
2020-11-10 09:55:44 +0000

    More.

--- _templates/admin.php
+++ _templates/admin.php
@@ -74,6 +74,40 @@
   </div>
 </div>
 
+
+<div class="modal fade" id="statsModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Statistics</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        forthcoming.
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Edit</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        forthcoming.
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <script>
 
 $.ajax({
--- _web/_content/links.php
+++ _web/_content/links.php
@@ -20,45 +20,53 @@
   <div class="alert alert-warning" role="alert">QR code deleted.</div>
 <?php } ?>
 
-<table class="table">
+<style>
+.ellipsis {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    max-width: 300px;
+}
+
+a.link { border-bottom: 1px solid #6c757d; color: #6c757d; }
+a.link:hover { text-decoration: none; border-bottom: 1px solid red; color: red; }
+</style>
+<table class="table table-hover">
 <thead>
   <tr>
     <th scope="col"></th>
     <th scope="col">Short URL</th>
-    <th scope="col">Forwarding URL</th>
-    <th scope="col">Scans</th>
+    <th scope="col">Forwarding To</th>
+    <th scope="col" style="width: 50px;">Scans</th>
     <th scope="col"></th>
   </tr>
 </thead>
 <tbody>
 <?php for ($x=0;$x<sizeof($links);$x++) { ?>
 <tr<?php if (isset($_POST['new']) && $_POST['new'] == 1 && $x == 0) { echo " class='table-warning'"; }?>>
-  <td>
-    <a class="btn btn-sm btn-outline-secondary" href="#" data-toggle="modal" data-target="#QRModal" data-uri="<?php echo $links[$x]['uri'];?>">QR code</a>
-    <a href="https://filter.ar/<?php echo $links[$x]['uri'];?>" data-url="https://filter.ar/<?php echo $links[$x]['uri'];?>" class="copyurl btn btn-sm btn-outline-secondary"><i class="far fa-copy"></i></a></td>
-  <td><a href="https://filter.ar/<?php echo $links[$x]['uri'];?>" target="_new"><?php echo $links[$x]['title'];?></a><br />
-  <td><a href="<?php echo $links[$x]['link']?>" target="_new" class="btn btn-sm btn-outline-secondary"><i class="fas fa-external-link-alt"></i></a></td>
-  <td><?php echo $links[$x]['usage']?></td>
-  <td class="text-right">
+  <td nowrap>
+    <a class="btn btn-sm btn-outline-secondary" href="#" data-toggle="modal" data-target="#QRModal" data-uri="<?php echo $links[$x]['uri'];?>">QR</a>
+    <a class="btn btn-sm btn-outline-secondary" href="#" data-toggle="modal" data-target="#editModal" data-uri="<?php echo $links[$x]['uri'];?>"><i class="far fa-edit"></i></a>
+    </td>
+  <td class="ellipsis" nowrap><a href="https://filter.ar/<?php echo $links[$x]['uri'];?>" data-url="https://filter.ar/<?php echo $links[$x]['uri'];?>" class="copyurl btn btn-sm btn-outline-secondary"><i class="far fa-copy"></i></a>&nbsp;<a class="link" href="https://filter.ar/<?php echo $links[$x]['uri'];?>" target="_new"><?php echo $links[$x]['title'];?></a><br />
+  <td class="ellipsis" nowrap><a class="link" href="<?php echo $links[$x]['link']?>" target="_new"><?php echo $links[$x]['link'];?></a></td>
+  <td style="width: 50px;"><?php echo $links[$x]['usage']?></td>
+  <td class="text-right" nowrap>
     <div class="btn-group">
       <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         Download
       </button>
       <div class="dropdown-menu">
-        <a class="dropdown-item" href="#">Edit</a>
-        <div class="dropdown-divider"></div>
 
-        <div class="dropdown-divider"></div>
         <a class="dropdown-item" download href="/qr/<?php echo $links[$x]['uri']?>.png">.PNG</a>
         <a class="dropdown-item" download href="/qr/<?php echo $links[$x]['uri']?>.svg">.SVG</a>
         <a class="dropdown-item" download href="/qr/<?php echo $links[$x]['uri']?>.eps">.EPS</a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="#">View Statistics</a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="/admin/?delete=<?php echo $links[$x]['id']; ?>">Delete</a>
 
       </div>
-    </div></td>
+    </div>
+<a class="btn btn-sm btn-outline-secondary" href="#" data-toggle="modal" data-target="#statsModal" data-uri="<?php echo $links[$x]['uri'];?>"><i class="fas fa-chart-line"></i></a>
+  <a class="btn btn-sm btn-outline-danger" href="/admin/?delete=<?php echo $links[$x]['id']; ?>"><i class="far fa-trash-alt"></i></a>
+</td>
 </tr>
 <?php } ?>
 </tbody>
@@ -106,6 +114,7 @@ $('body').on('click','.copyurl',function(e) {
   e.preventDefault();
   copyTextToClipboard($(this).data('url'));
   $(this).css('color','red');
+  $(this).css('border','1px solid red');
 });
 
 
